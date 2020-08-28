69a9e67358c39fc2fb80d0af8600278805a3f21c
Evgenyi Yakimchuk <Kum4yk@gmail.com>
2020-08-28 20:49:31 +0700

    done 4 week

--- Specialization. ML and Data Analysis/6_final_project/4. Feedback tone analysis/04 algorithm demo/demo.py
+++ Specialization. ML and Data Analysis/6_final_project/4. Feedback tone analysis/04 algorithm demo/demo.py
@@ -1,10 +1,15 @@
-__author__ = 'xead'
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SubmitField
+from wtforms.validators import Length, DataRequired
 from sentiment_classifier import SentimentClassifier
-from codecs import open
 import time
-from flask import Flask, render_template, request
-app = Flask(__name__)
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret_key"
+bootstrap = Bootstrap(app)
 print("Preparing classifier")
 start_time = time.time()
 classifier = SentimentClassifier()
@@ -12,21 +17,20 @@ print("Classifier is ready")
 print(time.time() - start_time, "seconds")
 
 
-@app.route("/sentiment-demo", methods=["POST", "GET"])
-def index_page(text="", prediction_message=""):
-    if request.method == "POST":
-        text = request.form["text"]
-        logfile = open("ydf_demo_logs.txt", "a", "utf-8")
-        print(text)
-        print(logfile, "<response>")
-        print(logfile, text)
-        prediction_message = classifier.get_prediction_message(text)
-        print(prediction_message)
-        print(logfile, prediction_message)
-        print(logfile, "</response>")
-        logfile.close()
-    return render_template('hello.html', text=text, prediction_message=prediction_message)
+class NameForm(FlaskForm):
+    name = TextAreaField("Отзыв", validators=[DataRequired(), Length(1, 5000)])
+    submit = SubmitField('Оценить')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=False)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        text = form.name.data
+        name = classifier.get_prediction_message(text)
+    return render_template('index.html', form=form, name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5050)
--- Specialization. ML and Data Analysis/6_final_project/4. Feedback tone analysis/04 algorithm demo/sentiment_classifier.py
+++ Specialization. ML and Data Analysis/6_final_project/4. Feedback tone analysis/04 algorithm demo/sentiment_classifier.py
@@ -1,44 +1,46 @@
-__author__ = 'xead'
-from sklearn.externals import joblib
+import joblib
+import numpy as np
 
 
 class SentimentClassifier(object):
     def __init__(self):
-        self.model = joblib.load("./DefaultLogisticBigramUnprocessedTextSentiment.pkl")
-        self.vectorizer = joblib.load("./BigramUnprocessedVectorizer.pkl")
+        self.model = joblib.load("pipeline_model.pkl")
         self.classes_dict = {0: "negative", 1: "positive", -1: "prediction error"}
 
     @staticmethod
     def get_probability_words(probability):
         if probability < 0.55:
             return "neutral or uncertain"
-        if probability < 0.7:
+        if probability < 0.75:
             return "probably"
-        if probability > 0.95:
-            return "certain"
+        if probability < 0.85:
+            return "mostly"
         else:
-            return ""
+            return "certainly"
 
-    def predict_text(self, text):
-        try:
-            vectorized = self.vectorizer.transform([text])
-            return self.model.predict(vectorized)[0],\
-                   self.model.predict_proba(vectorized)[0].max()
-        except:
-            print("prediction error")
-            return -1, 0.8
 
-    def predict_list(self, list_of_texts):
-        try:
-            vectorized = self.vectorizer.transform(list_of_texts)
-            return self.model.predict(vectorized),\
-                   self.model.predict_proba(vectorized)
-        except:
-            print('prediction error')
-            return None
+    def predict_text(self, text: str):
+        if not isinstance(text, list) or not isinstance(text, np.ndarray):
+            text = [text]
+        pred_class = self.model.predict(text)[0]
+        prob_pred_class = self.model.predict_proba(text)[0][pred_class]
+        return pred_class, prob_pred_class
 
     def get_prediction_message(self, text):
         prediction = self.predict_text(text)
         class_prediction = prediction[0]
         prediction_probability = prediction[1]
-        return self.get_probability_words(prediction_probability) + " " + self.classes_dict[class_prediction]
+
+        percent = round(prediction_probability * 100, 2)
+        word = self.get_probability_words(prediction_probability).capitalize()
+        pred_class = self.classes_dict[class_prediction]
+
+        return f"{word} sure ({percent}%) that the review is {pred_class}."
+
+
+if __name__ == "__main__":
+    check = SentimentClassifier()
+    test_text = "very bad phone"
+    print(check.predict_text(test_text))
+    print(check.get_prediction_message(test_text))
+
